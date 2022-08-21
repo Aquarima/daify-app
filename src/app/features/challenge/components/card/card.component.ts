ddc0188008438a8f8fe7314d5213b72e6c805b10
Exalow <exalow.one@gmail.com>
2022-08-21 19:52:18 +0200

    Make card tags bg to the same color that challenge cover dominant color

--- src/app/features/challenge/components/card/card.component.ts
+++ src/app/features/challenge/components/card/card.component.ts
@@ -1,5 +1,6 @@
-import { AfterViewInit, Component, ComponentRef, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { AfterViewInit, Component, ComponentRef, ElementRef, Input, OnInit, ViewChild } from '@angular/core';
 import { FastAverageColor } from 'fast-average-color';
+import { Challenge } from 'src/app/core/models/challenge';
 
 @Component({
   selector: 'app-challenge-card',
@@ -8,16 +9,18 @@ import { FastAverageColor } from 'fast-average-color';
 })
 export class CardComponent implements OnInit, AfterViewInit {
 
-  @ViewChild('tag_list') tagList: ElementRef | undefined;
+  @Input() challenge: Challenge | undefined
+  @ViewChild('tag_list') tagList: ElementRef | undefined
 
   constructor() { }
 
-  ngOnInit(): void { }
+  ngOnInit(): void {}
 
   ngAfterViewInit(): void {
+    const coverUrl = this.challenge?.coverUrl;
+    if (!coverUrl) return;
     const fac = new FastAverageColor();
-    const image = 'https://images.unsplash.com/photo-1481349518771-20055b2a7b24?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8cmFuZG9tfGVufDB8fDB8fA%3D%3D&w=1000&q=80';
-    fac.getColorAsync(image)
+    fac.getColorAsync(coverUrl)
       .then(color => {
         if (!this.tagList) return;
         const tagNodes = this.tagList.nativeElement.children;
