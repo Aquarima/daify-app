fedb5e1380f999f6f2e6e3068f68bc4c6361219f
Exalow <exalow.one@gmail.com>
2022-08-21 19:48:02 +0200

    Separate user interface in two parts

---
+++ src/app/core/models/user/profile.model.ts
@@ -0,0 +1,10 @@
+export interface Profile {
+    firstName: string
+    lastName: string
+    username: string
+    avatarUrl: string
+    country: string
+    birthday: Date
+    lastLogin: Date
+    dateCreated: Date
+}
--- src/app/core/models/user/user.model.ts
+++ src/app/core/models/user/user.model.ts
@@ -1,12 +1,5 @@
 export interface User {
     email: String
     password: string
-    firstName: string
-    lastName: string
-    username: string
-    avatarUrl: URL
-    country: string
-    birthday: Date
-    lastLogin: Date
-    creationDate: Date
+    dateCreated: Date
 }
