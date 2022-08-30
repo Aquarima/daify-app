fde2268553eec1947f05c68bfbecc6f42f9855ef
Exalow <exalow.one@gmail.com>
2022-08-30 20:11:18 +0200

    Update imports/exports

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,11 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ChallengeService } from './services/challenge.service';
+import { AuthService, SearchService } from './services';
+import { HTTP_INTERCEPTORS } from '@angular/common/http';
+import { JwtInterceptor } from './interceptors/jwt.interceptor';
 
 @NgModule({
   declarations: [],
   providers: [
+    AuthService,
     ChallengeService,
+    SearchService,
+    { provide: HTTP_INTERCEPTORS, useClass: JwtInterceptor, multi: true }
   ],
   imports: [
     CommonModule,
