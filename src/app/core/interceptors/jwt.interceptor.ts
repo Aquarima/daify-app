d005cab245272d99d289f21f3a514f0bcbaef1ca
Exalow <exalow.one@gmail.com>
2022-08-30 14:45:30 +0200

    Create Jwt interceptor

---
+++ src/app/core/interceptors/jwt.interceptor.ts
@@ -0,0 +1,35 @@
+import { Injectable } from '@angular/core';
+import {
+  HttpRequest,
+  HttpHandler,
+  HttpEvent,
+  HttpInterceptor,
+  HttpResponse
+} from '@angular/common/http';
+import { catchError, Observable, throwError } from 'rxjs';
+import { AuthService } from '../services';
+import { Router } from '@angular/router';
+
+@Injectable()
+export class JwtInterceptor implements HttpInterceptor {
+
+  constructor(private router: Router, private authService: AuthService) { }
+
+  intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
+
+    if (request.url.startsWith('http://localhost:8080') && this.authService.isAuthenticated()) {
+      request = request.clone({
+        setHeaders: {Authorization: `${this.authService.getToken()}`}
+      });
+    }
+
+    return next.handle(request).pipe(
+      catchError((err) => {
+        if (err instanceof HttpResponse && err.status === 401) {
+          this.router.navigate(['/auth/login']);
+        }
+        return throwError(() => new Error(err));
+      })
+    )
+  }
+}
