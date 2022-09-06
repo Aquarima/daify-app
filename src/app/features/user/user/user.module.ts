35b9cb0e356fbf95b0cc194d568b90089d9b9007
Exalow <exalow.one@gmail.com>
2022-09-07 00:52:37 +0200

    Update styles and profile component

--- src/app/features/user/user/user.module.ts
+++ src/app/features/user/user/user.module.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 
 import { UserRoutingModule } from './user-routing.module';
 import { ProfileComponent } from './pages/profile/profile.component';
+import { LayoutModule } from 'src/app/layout/layout.module';
 
 
 @NgModule({
@@ -11,7 +12,8 @@ import { ProfileComponent } from './pages/profile/profile.component';
   ],
   imports: [
     CommonModule,
-    UserRoutingModule
+    UserRoutingModule,
+    LayoutModule,
   ]
 })
 export class UserModule { }
