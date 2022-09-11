44f63983a027905fef82c0a85df8c3bd7ccf3113
Exalow <exalow.dev@gmail.com>
2022-09-11 15:50:37 +0200

    Create UserCard and UserList components

---
+++ src/app/features/user/components/user-card/user-card.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'dfy-user-card',
+  templateUrl: './user-card.component.html',
+  styleUrls: ['./user-card.component.scss']
+})
+export class UserCardComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void { }
+}
