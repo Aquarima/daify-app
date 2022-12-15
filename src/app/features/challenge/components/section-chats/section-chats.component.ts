fc2706a438ddc74518c5e10eda000b36f4df50bc
Exalow <exalow.dev@gmail.com>
2022-12-16 00:23:14 +0100

    Clean up

--- src/app/features/challenge/components/section-chats/section-chats.component.ts
+++ src/app/features/challenge/components/section-chats/section-chats.component.ts
@@ -1,4 +1,4 @@
-import {Component, ElementRef, Input, OnInit, QueryList, ViewChildren} from '@angular/core';
+import {Component, ElementRef, Input, NgZone, OnInit, QueryList, ViewChildren} from '@angular/core';
 import {AuthService, Challenge} from "../../../../core";
 import {ChannelService} from "../../../../core/services/channel.service";
 import {MessageService} from "../../../../core/services/message.service";
@@ -11,90 +11,91 @@ import {Observable} from "rxjs";
 import {FormControl, FormGroup} from "@angular/forms";
 
 @Component({
-    selector: 'dfy-challenge-chats',
-    templateUrl: './section-chats.component.html',
-    styleUrls: ['./section-chats.component.scss']
+  selector: 'dfy-challenge-chats',
+  templateUrl: './section-chats.component.html',
+  styleUrls: ['./section-chats.component.scss']
 })
 export class SectionChatsComponent implements OnInit {
 
-    @ViewChildren('text_input') textInputs!: QueryList<ElementRef>;
+  @ViewChildren('text_input') textInputs!: QueryList<ElementRef>;
 
-    @Input() section!: Observable<number>;
-    @Input() challenge!: Challenge;
-    @Input() selfMember!: Member;
+  @Input() section!: Observable<number>;
+  @Input() challenge!: Challenge;
+  @Input() selfMember!: Member;
 
-    userInputForm = new FormGroup({
-        message: new FormControl(''),
-    })
+  userInputForm = new FormGroup({
+    message: new FormControl(''),
+  })
 
-    channels: Channel[] = [];
-    currentChannel: Channel | undefined;
-    cachedMessages: Map<Channel, Array<Message>> = new Map();
-    messages: Array<Message> = [];
+  channelCache: Channel[] = [];
+  messageCache: Map<Channel, Array<Message>> = new Map();
 
-    constructor(
-        private alertHandlingService: AlertHandlingService,
-        private authService: AuthService,
-        private channelService: ChannelService,
-        private messageService: MessageService) {
-    }
+  channel: Channel | undefined;
+  messages: Message[] = [];
 
-    ngOnInit(): void {
-        this.channelService.getChannelsByChallenge(this.challenge.id).subscribe({
-            next: (data: any) => {
-                this.channels = data.content;
-                if (!this.currentChannel && this.channels.length > 0) {
-                    this.setCurrentChannel(this.channels[0]);
-                }
-            },
-            error: () => this.alertHandlingService.throwAlert(AlertType.ERROR, 'Could not fetch channels'),
+  constructor(
+    private ngZone: NgZone,
+    private alertHandlingService: AlertHandlingService,
+    private authService: AuthService,
+    private channelService: ChannelService,
+    private messageService: MessageService) {
+  }
+
+  ngOnInit(): void {
+    this.channelService.getChannelsByChallenge(this.challenge.id)
+      .subscribe({
+        next: (channels: any) => {
+          this.channelCache = channels.content;
+          if (!this.channel && this.channelCache.length > 0) {
+            this.onChannelSelected(this.channelCache[0]);
+          }
+        },
+        error: (err: any) => this.alertHandlingService.throwAlert(AlertType.ERROR, err),
+      })
+  }
+
+  ngAfterViewInit(): void {
+    this.initTextInputsListeners();
+  }
+
+  private initTextInputsListeners() {
+    this.textInputs.forEach(input => {
+      const inputElement = input.nativeElement;
+      const parentNode = inputElement.parentNode;
+      inputElement.addEventListener('focus', () => {
+        parentNode.classList.add('user-input-active')
+      });
+      inputElement.addEventListener('focus', () => {
+        parentNode.classList.add('user-input-active')
+      });
+      inputElement.addEventListener('focusout', () => {
+        if (inputElement.value === '') parentNode.classList.remove('user-input-active');
+      });
+    });
+  }
+
+  loadChat(channel: Channel) {
+    if (this.messageCache.has(channel)) return;
+    this.ngZone.run(() => {
+      this.messageService.getMessagesByChannel(channel.id)
+        .subscribe({
+          next: (messages: any) => this.messageCache.set(channel, messages.content),
+          error: (err: any) => this.alertHandlingService.throwAlert(AlertType.ERROR, err),
         })
-    }
+    })
+  }
 
-    ngAfterViewInit(): void {
-        this.initTextInputsListeners();
-    }
+  onChannelSelected(channel: Channel) {
+    this.loadChat(channel);
+    this.messages = this.messageCache.get(channel) || [];
+    this.channel = channel;
+  }
 
-    private initTextInputsListeners() {
-        this.textInputs.forEach(input => {
-            const inputElement = input.nativeElement;
-            const parentNode = inputElement.parentNode;
-            inputElement.addEventListener('focus', () => {
-                parentNode.classList.add('user-input-active')
-            });
-            inputElement.addEventListener('focus', () => {
-                parentNode.classList.add('user-input-active')
-            });
-            inputElement.addEventListener('focusout', () => {
-                if (inputElement.value === '') parentNode.classList.remove('user-input-active');
-            });
-        });
-    }
+  onSendMessage() {
 
-    onSendMessage() {
-        if (!this.currentChannel) return;
-        const rawMessage: string = `${this.userInputForm.controls.message.value}`;
-        const message: Message = {id: 0, sender: {} as Member, content: rawMessage, sent_at: new Date()};
-        this.messageService.sendMessage(this.currentChannel.id, message)
-            .subscribe({
-                next: (message: any) => this.messages.push(message),
-                error: () => this.alertHandlingService.throwAlert(AlertType.ERROR, 'Could not send message'),
-            })
-        this.userInputForm.controls.message.setValue('');
-    }
+  }
 
-    setCurrentChannel(channel: Channel) {
-        this.currentChannel = channel;
-        if (!this.cachedMessages.has(channel)) {
-            this.messageService.getMessagesByChannel(channel.id).subscribe({
-                next: (data: any) => this.cachedMessages.set(channel, data.content),
-                error: () => this.alertHandlingService.throwAlert(AlertType.ERROR, 'Could not fetch messages'),
-            })
-        }
-        this.messages = this.cachedMessages.get(channel) || [];
-    }
-
-    isSelfMember(member: Member): boolean {
-        return member.profile.id == this.authService.user.profile.id;
-    }
+  isSelfMember(member: Member): boolean {
+    return member.profile.id == this.authService.user.profile.id;
+  }
 }
