239e031019be6c3b5c39a8afc988cda172d76d77
Exalow <exalow.dev@gmail.com>
2023-01-24 08:53:36 +0100

    Fix service

--- src/app/core/services/notification.service.ts
+++ src/app/core/services/notification.service.ts
@@ -1,33 +1,37 @@
 import {Injectable} from '@angular/core';
 import {HttpClient} from "@angular/common/http";
 import {environment as env} from "../../../environments/environment";
-import {Notification, User} from "../models";
+import {User} from "../models";
 
 @Injectable({
-    providedIn: 'root'
+  providedIn: 'root'
 })
 export class NotificationService {
 
-    constructor(private http: HttpClient) {
-        console.log(this.interpret('Visit our page at ${LINK:link:https://daify.fr/app/welcome}'));
-    }
+  constructor(private http: HttpClient) {
+    console.log(this.interpret('Visit our page at ${LINK+link+https://daify.fr/app/welcome}'));
+  }
 
-    getNotificationsByUser(user: User) {
-        return this.http.get<any>(`${env.apiUrl}/user/${user.id}/notification?order=desc`);
-    }
+  getNotificationsByUser(user: User) {
+    return this.http.get<any>(`${env.apiUrl}/user/${user.id}/notification?order=desc`);
+  }
 
-    interpret(body: string): string {
-        const args: string[] = body.split('');
-        let res = '<p>';
-        for (let i = 0; i < args.length; i++) {
-            const match: RegExpMatchArray | null = args[i].match('^\\$\\{[a-zA-Z0-9]+:[a-zA-Z0-9 ]+:[a-zA-Z0-9\\://]+\\}$');
-            if (match) {
-                const type = match[0].toUpperCase();
-                if (type === 'LINK') {
-                    res = res + `<a href="${match[1]}">${match[2]}</a>`;
-                }
-            }
+  interpret(body: string): string {
+    const args: string[] = body.split(' ');
+    let res = '<p>';
+    for (let i = 0; i < args.length; i++) {
+      const regex = '^\\$\\{.+\\+.+\\+.+\\}$';
+      const match: RegExpMatchArray | null = args[i].match(regex);
+      if (match) {
+        const split: string[] = match[0]
+          .replace('${', '')
+          .replace('}', '')
+          .split('+');
+        if (split[0] === 'LINK') {
+          res = res + `<a href="${split[2]}">${split[1]}</a>`;
         }
-        return res + '</p>';
+      }
     }
+    return res + '</p>';
+  }
 }
