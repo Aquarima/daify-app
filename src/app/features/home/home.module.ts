19b486c91ec46eb732324874815af6540b7e437c
Exalow <dev.exalow@gmail.com>
2023-02-01 11:59:46 +0100

    Create resource not found component

--- src/app/features/home/home.module.ts
+++ src/app/features/home/home.module.ts
@@ -4,11 +4,12 @@ import {CommonModule} from '@angular/common';
 import {HomeRoutingModule} from './home-routing.module';
 import {HomeComponent, MaintenanceComponent} from './pages';
 import {LayoutModule} from 'src/app/layout/layout.module';
-import { SplashScreenComponent } from './components/splash-screen/splash-screen.component';
+import { SplashScreenComponent } from './components';
+import { ResourceNotFoundComponent } from './pages/resource-not-found/resource-not-found.component';
 
 @NgModule({
   declarations: [
-    HomeComponent, MaintenanceComponent, SplashScreenComponent,
+    HomeComponent, MaintenanceComponent, SplashScreenComponent, ResourceNotFoundComponent,
   ],
   imports: [
     CommonModule,
