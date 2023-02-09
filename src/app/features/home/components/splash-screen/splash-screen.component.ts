56eadf82e8d5abf019fb3e53fd8fb681e0134152
Exalow <dev.exalow@gmail.com>
2023-02-09 19:51:03 +0100

    Improve login and sign up UI

--- src/app/features/home/components/splash-screen/splash-screen.component.ts
+++ src/app/features/home/components/splash-screen/splash-screen.component.ts
@@ -3,26 +3,26 @@ import {DOCUMENT} from "@angular/common";
 import {DomSanitizer} from "@angular/platform-browser";
 
 @Component({
-  selector: 'dfy-splash-screen',
-  templateUrl: './splash-screen.component.html',
-  styleUrls: ['./splash-screen.component.scss']
+    selector: 'dfy-splash-screen',
+    templateUrl: './splash-screen.component.html',
+    styleUrls: ['./splash-screen.component.scss']
 })
 export class SplashScreenComponent implements OnInit, AfterViewInit, OnDestroy {
 
-  constructor(
-      @Inject(DOCUMENT) private document: Document,
-      private renderer: Renderer2,
-      private sanitizer: DomSanitizer) {
-  }
+    constructor(
+        @Inject(DOCUMENT) private document: Document,
+        private renderer: Renderer2,
+        private sanitizer: DomSanitizer) {
+    }
 
-  ngOnInit(): void {
-  }
+    ngOnInit(): void {
+    }
 
-  ngAfterViewInit(): void {
-    this.renderer.addClass(this.document.body, 'no-scroll');
-  }
+    ngAfterViewInit(): void {
+        this.renderer.addClass(this.document.body, 'no-scroll');
+    }
 
-  ngOnDestroy(): void {
-    this.renderer.removeClass(this.document.body, 'no-scroll');
-  }
+    ngOnDestroy(): void {
+        this.renderer.removeClass(this.document.body, 'no-scroll');
+    }
 }
