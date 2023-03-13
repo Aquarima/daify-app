ae233872501ca26cc822c528bd3da942e77312e8
Exalow <exalow.dev@gmail.com>
2023-03-13 11:31:08 +0100

    Work on leaderboard UI
    - Add podiums and time remaining

--- src/app/features/challenge/components/section-leaderboard/section-leaderboard.component.ts
+++ src/app/features/challenge/components/section-leaderboard/section-leaderboard.component.ts
@@ -1,18 +1,42 @@
-import {Component, Input, OnInit} from '@angular/core';
+import {Component, Input, OnDestroy, OnInit} from '@angular/core';
 import {Challenge, Group} from "../../../../core";
+import {TimeHelper, TimeLeft} from "../../../../core/helpers";
+import {interval, Subscription} from "rxjs";
+import {EMPTY_SUBSCRIPTION} from "rxjs/internal/Subscription";
 
 @Component({
   selector: 'dfy-challenge-leaderboard',
   templateUrl: './section-leaderboard.component.html',
   styleUrls: ['./section-leaderboard.component.scss']
 })
-export class SectionLeaderboardComponent implements OnInit {
+export class SectionLeaderboardComponent implements OnInit, OnDestroy {
 
   @Input() challenge!: Challenge;
   @Input() groups: Group[] = [];
 
-  constructor() { }
+  countdownSubscription: Subscription = EMPTY_SUBSCRIPTION;
+  countdown: TimeLeft = {days: 7, hours: 0, minutes: 0, seconds: 0} as TimeLeft;
+
+  constructor(private timeHelper: TimeHelper) { }
 
   ngOnInit(): void {
+    this.countdownSubscription = interval(1000).subscribe(() => {
+      this.countdown = this.timeHelper.calculateTimeRemaining(new Date(Date.now() + 3600000));
+    });
+  }
+
+  ngOnDestroy() {
+    this.countdownSubscription.unsubscribe();
+  }
+
+  extract(number: number): string[] {
+    if (!number) return ['0', '0'];
+    const res: string[] = number < 10 ? ['0'] : [];
+    `${number}`.split('').forEach(i => res.push(i));
+    return res;
+  }
+
+  isChallengeEnded(): boolean {
+    return new Date(this.challenge.config.endAt).getTime() < Date.now();
   }
 }
